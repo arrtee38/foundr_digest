71581e46ba8d88242b29a3ff40082cf536d08081
arrtee38 <arrtee38@gmail.com>
2023-08-04 01:22:14 +0000

    added input validation and cleanup of apply and controllers

--- app/controllers/user_submissions_controller.rb
+++ app/controllers/user_submissions_controller.rb
@@ -1,11 +1,10 @@
 class UserSubmissionsController < ApplicationController
     def create
-        puts "WHITELISTED PARAMS: #{user_submission_params}"
-        # UserSubmission.create(params)
-        # accept the client request (form data) and create a new User Submission entry
+        UserSubmission.create!(user_submission_params)
+        redirect_to root_path, notice: 'Thanks! Your application was received.'
     end
 
     def user_submission_params
-        params.require(:user_submission).permit(:first_name, :last_name, :email, :website, :job_role, :text)
+        params.require(:user_submission).permit(:first_name, :last_name, :email, :website, :job_role, :text, :plan_name)
     end
 end
