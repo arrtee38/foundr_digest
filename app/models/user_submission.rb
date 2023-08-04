49c7697186c7bbb6ccca295b4b829f5456943289
arrtee38 <arrtee38@gmail.com>
2023-08-04 01:23:20 +0000

    updated user submission class

--- app/models/user_submission.rb
+++ app/models/user_submission.rb
@@ -1,2 +1,6 @@
 class UserSubmission < ApplicationRecord
+    PLAN_NAMES = ['free', 'pro']
+
+    validates :plan_name, inclusion: { in: PLAN_NAMES }
+    validates_presence_of :first_name, :last_name, :email, :website, :job_role, :text
 end
